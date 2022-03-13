27b9101fc9c1be97714e6dbfef017be038419835
Lukas Felser <91831889+lukasfelser@users.noreply.github.com>
2022-03-13 22:22:37 +0100

    Add files via upload

---
+++ AlgoDat_week1.py
@@ -0,0 +1,43 @@
+
+def name_age(x=input("What is your name?"), y=input("How old are you?")):
+    print(f"Hello {x}. Your age is {y}.")                                           # weiß nicht, ob richtig so
+    return x + y
+
+
+def swap_integers(num1, num2):
+    x = str(num1)
+    y = str(num2)
+    print(y, x)
+    return y + x
+
+
+def check_numbers(num1, num2):
+    return (num1 % 10 == 0 and num2 % 10 == 0) and num1 % 6 == 0 or num2 % 6 == 0   # einzeilige Lösung, lange Zeile
+
+
+def sum_up(num1, num2):
+    result = 0
+    if num2 < num1 or (num1 < 0 or num2 < 0):
+        return 0
+    for num in range(num1, num2+1):
+        result += num
+    return result
+
+
+def circle_area(r1, r2, r3):
+    c1 = 3.14 * (r1 ** 2)
+    c2 = 3.14 * (r2 ** 2)
+    c3 = 3.14 * (r3 ** 2)
+    return c1 + c2 + c3
+
+
+def check_string(text):
+    text = text.lower()
+    return text.startswith('a') or text.endswith('a')
+
+
+def triangle(rows):
+    for x in range(1, rows + 1):
+        for y in range(1, x + 1):
+            print('* ', end='')
+        print(' ')
